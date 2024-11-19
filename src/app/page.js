76be68e582335aaf9b12b0edb9e2a43dfe501f6e
AsTeriaa09 <mahiraakhter950@gmail.com>
2024-11-19 23:57:01 +0600

    basic interface design and add states

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,37 @@
-
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [prompt, setPrompt] = useState("");
+  const [convoHistory, setConvoHistory] = useState([]);
+  const [response, setResponse] = useState("");
+  const [error, setError] = useState("");
+
+  const handleResponse = () => {};
+
   return (
-   <>
-    chat
-   </>
+    <>
+      <div className="flex flex-col h-screen bg-zinc-900">
+        {/* chat logic */}
+
+        <div className="flex items-center p-4 bg-zinc-900 ">
+          <input
+            type="text"
+            placeholder="Type your message..."
+            value={prompt}
+            onChange={(e) => {
+              setPrompt(e.target.value);
+            }}
+            className="flex-1 p-2 me-2 border bg-zinc-800 border-zinc-900 text-white rounded-l-md focus:outline-none focus:ring-2 "
+          />
+          <button
+            className="p-2 bg-neutral-100 text-black rounded-md "
+            onClick={handleResponse}
+          >
+            Generate Response
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
